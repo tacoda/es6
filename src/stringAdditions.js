ec0e23246b6ae71367de8309452dc6907a5afb12
Ian Johnson <tacoda@pm.me>
2019-02-02 20:32:50 -0500

    Complete string additions

---
+++ src/stringAdditions.js
@@ -0,0 +1,15 @@
+let title = 'Red Rising';
+
+if (! title.includes('Blue')) {
+  console.log('This book does not include Blue');
+}
+
+if (title.startsWith('Red')) {
+  console.log('This book starts with Red');
+}
+
+if (title.endsWith('ing')) {
+  console.log('It does');
+}
+
+console.log('lol'.repeat(100));
