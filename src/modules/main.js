abd05aebeb900ad6d03e3444da6ee4edb6ddcab5
Ian Johnson <tacoda@pm.me>
2019-02-02 19:37:06 -0500

    Complete modules lesson

---
+++ src/modules/main.js
@@ -0,0 +1,8 @@
+// import { TaskCollection } from './TaskCollection';
+import TaskCollection from './TaskCollection';
+
+new TaskCollection([
+  'Go to the store',
+  'Finish screencast',
+  'Eat cake'
+]).dump();
